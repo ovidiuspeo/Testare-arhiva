301ce3fe0b1c1bdb65b43f90be0bdba90dddc3a4
ovidiuspeo <ovidiu_dolcan@yahoo.com>
2026-05-29 21:20:50 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,11 +1,1 @@
-console.log("Se inițializează Supabase...");
 
-const SUPABASE_URL = "https://supabase.co";
-const SUPABASE_KEY = "sb_publishable_XQjrq5LLhA4b3O32ioObqQ_PNbJD5E5";
-
-try {
-    window.db_supa = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-    console.log("Conexiune creată cu succes în window.db_supa");
-} catch (e) {
-    console.error("Eroare la crearea clientului Supabase:", e);
-}
